37384916140906c3f20022d059bee9d542e03981
mandersen21 <mikkelandersen21@gmail.com>
2018-12-28 21:34:10 +0100

    Added fix.

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
+const bodyParser = require("body-parser");
+const config = require('config');
+const winston = require('winston');
 const PythonShell = require('python-shell');
+
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/predict', getPrediction);
 
@@ -8,8 +17,16 @@ function getPrediction(req, res) {
 
     PythonShell.run('./python.py', function (err, data) {
         if (err) res.send(err);
-        res.json(data)
+        if (data) {
+            var prediction = data.toString()
+            prediction = prediction.split("[")[1].split("]")[0]
+            res.send(prediction)
+        }
+        else res.send(null)
     });
 }
 
-app.listen(4000, () => console.log('Application listening on port 4000!'))
+const port = process.env.PORT || 4000;
+const server = app.listen(port, () => winston.info('Listening on port ' + port))
+
+module.exports = server;
